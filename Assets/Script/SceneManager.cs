1667084418f40620de82a022471e9b28209c5efd
GotohFumiaki <gf1010redgreen@gmail.com>
2019-02-17 19:34:18 +0900

    IStateProviderインターフェース追加

--- Assets/Script/SceneManager.cs
+++ Assets/Script/SceneManager.cs
@@ -9,13 +9,11 @@ namespace States
         // Start is called before the first frame update
         void Start()
         {
-            
         }
 
         // Update is called once per frame
         void Update()
         {
-            
         }
     }
 }
